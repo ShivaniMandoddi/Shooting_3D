4cc80e82261a3f12b70d671e3e2db5890e377f95
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-03-14 13:11:58 +0530

    Score is Added

--- Assets/ScoreCalcutor.cs
+++ Assets/ScoreCalcutor.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCalcutor : MonoBehaviour
 {
     // Start is called before the first frame update
     public int score;
+    public Text scoreText;
     public void Score(int scoreValue)
     {
         score = score + scoreValue;
+        
+        scoreText.text ="Score: " +score.ToString();
         Debug.Log(score);
 
     }
